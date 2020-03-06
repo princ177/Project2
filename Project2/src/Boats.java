cdf507cd4b0d160c044b8da1aca0c264778efcb0
Varun Reddy <reddy215@umn.edu>
2020-03-05 20:45:48 -0600

    beginning

---
+++ Project2/src/Boats.java
@@ -0,0 +1,11 @@
+package project2;
+
+public class Boats {
+	int length;
+	public Boats(int length) {
+		this.length = length;
+	}
+	public static void main(String[] args) {
+		System.out.print("test");
+	}
+}
